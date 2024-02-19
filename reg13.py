26c38969766e599cf9908ca3575c94e7be938ba6
tvio <tvio@centrum.cz>
2024-02-19 15:24:29 +0100

    delete jde, prosel test bez kodu pracoviste

--- reg13.py
+++ reg13.py
@@ -30,15 +30,31 @@ pracoviste = '00150017164'
 url = 'https://testapi.sukl.cz/reg13/v3'   
 
 
+def auth():
+ with pfx_to_pem('MAHSUKL150017166G.pfx', 'Test1234') as  c:
+  global cert
+  cert = c 
+
+def doReq(htmlOperace,operace):
+  if htmlOperace=='get':
+   res = requests.get(url+operace,cert=cert,timeout=5)
+  elif htmlOperace=='post':
+   res = requests.post(url+operace,cert=cert,timeout=5)
+  elif htmlOperace=='delete':
+   res = requests.delete(url+operace,cert=cert,timeout=5)
+  elif htmlOperace=='put':
+   res = requests.put(url+operace,cert=cert,timeout=5)
+  
+  return res
 def loadJSON(soubor):
     with  open(soubor,'r') as f:
      data = json.load(f)
      return data
 
 #nacte privni kod pracoviste podle typu
-def nactiKodPracoviste(typ,ico=None,kodPracoviste=None):
-  with pfx_to_pem('MAHSUKL150017166G.pfx', 'Test1234') as cert:
-     res = requests.get(url+'/pracoviste/'+str(typ),cert=cert)
+def nactiKodPracoviste(typ,**kwargs):
+     if
+     res = doReq('get','/pracoviste/'+str(typ))
      resjson = res.json()
      firstKod = resjson[0]['kodPracoviste']
      return firstKod
@@ -49,16 +65,17 @@ def printRes(operace,res,**kwargs):
       print('req>>'+str(kwargs.get("req")))
    print('res>>'+res.text)
    print('statusCode>>'+str(res.status_code))
-   
+
+#delete s getem Id   
 def delete(rok,mesic):
- with pfx_to_pem('MAHSUKL150017166G.pfx', 'Test1234') as cert:
-   res = requests.get(url+'/hlaseni/'+pracoviste+'/rok/'+rok+'/mesic/'+mesic)
-   print(res.)
-   res =  requests.delete(url+'/hlaseni'+str(podaniID), cert=cert )
+   res = doReq('get','/hlaseni/'+pracoviste+'/rok/'+rok+'/mesic/'+mesic)
+   printRes('getID podle data a kodu pracoviste',res)
+   resjson = res.json()[0]
+   res =  doReq('delete','/hlaseni/'+resjson )
    printRes('deleteHlaseni',res)
 
 #mainUC
-def sKodemPrac(typ,mesic):
+def sKodemPrac(obdobi):
   
   with pfx_to_pem('MAHSUKL150017166G.pfx', 'Test1234') as cert:
    #status
@@ -68,7 +85,7 @@ def sKodemPrac(typ,mesic):
    postHlaseniJSON = loadJSON('postHlaseni.json')
    postHlaseniJSON["podaniID"]  = str(uuid.uuid4())
    postHlaseniJSON['reglp'][0]["polozkaID"] = str(uuid.uuid4())
-   postHlaseniJSON["obdobi"] = mesic
+   postHlaseniJSON["obdobi"] = obdobi
    if typ=='dis':
      postHlaseniJSON['reglp'][0]["kodPracoviste"] = nactiKodPracoviste(2)
      postHlaseniJSON['reglp'][0]["typHlaseni"] = (1)
@@ -78,7 +95,7 @@ def sKodemPrac(typ,mesic):
    res = requests.post(url+'/hlaseni',cert=cert,json=postHlaseniJSON)
    printRes('postHlaseni',res,req=postHlaseniJSON)
 #test bez kodu pracoviste   
-def bezKoduPrac(typ,mesic):
+def bezKoduPrac(mesic):
   
   with pfx_to_pem('MAHSUKL150017166G.pfx', 'Test1234') as cert:
    #status
@@ -89,16 +106,17 @@ def bezKoduPrac(typ,mesic):
    postHlaseniJSON["podaniID"]  = str(uuid.uuid4())
    postHlaseniJSON['reglp'][0]["polozkaID"] = str(uuid.uuid4())
    postHlaseniJSON["obdobi"] = mesic
-   if typ=='dis':
-     postHlaseniJSON['reglp'][0]["kodPracoviste"] = ''
-     postHlaseniJSON['reglp'][0]["typHlaseni"] = (1)
-   elif typ=='lek':
-     postHlaseniJSON['reglp'][0]["kodPracoviste"] = ''
-     postHlaseniJSON['reglp'][0]["typHlaseni"] = (2) 
+   postHlaseniJSON.pop("kodPracoviste")  
+   #postHlaseniJSON["kodPracoviste"]=""
    res = requests.post(url+'/hlaseni',cert=cert,json=postHlaseniJSON)
    printRes('postHlaseni',res,req=postHlaseniJSON)
    
-
+auth()
+#vezme prvni pracoviste dis z ciselniku praocvist
 #mainUC(typ='dis',mesic='202403')
+#vezme prvni pracoviste lek z ciselniku pracovist
 #mainUC(typ='lek',mesic='202404')
-bezKodu
+#smaze hlaeni za konrketni mesic
+#delete('2024','03');
+#bezKoduPrac('202404')
+
